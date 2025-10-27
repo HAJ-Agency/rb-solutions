eba89bc3fa74a468e4b032a4f0c8ebd76de68ac1
Kim Harrington Jansson <hello@hajagency.com>
2025-10-27 09:51:38 +0100

    Desktop menu done (except animation submenu open)

--- src/js/header.js
+++ src/js/header.js
@@ -1,10 +1,12 @@
 (function ($) {
   $(function () {
     $(".menu-dropdown-show a").hover(function () {
+      $('.menu-dropdown-show').addClass('active');
       $(".menu-dropdown").addClass("shown");
     });
   });
   $(document).on("mouseleave", "header:has(.menu-dropdown.shown)", function () {
+    $('.menu-dropdown-show').removeClass('active');
     $(".menu-dropdown").removeClass("shown");
   });
 })(jQuery);
