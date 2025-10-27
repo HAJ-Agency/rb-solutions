8329a0002f7e20c87df235fd69bcef9349892e0a
Kim Harrington Jansson <hello@hajagency.com>
2025-10-27 08:32:50 +0100

    Footer development.

--- src/js/header.js
+++ src/js/header.js
@@ -1,10 +1,10 @@
 (function ($) {
-   $(function () {
-      $(".menu-dropdown-show a").hover(function () {
-         $(".menu-dropdown").addClass("shown");
-      });
-   });
-   $(document).on("mouseleave", "header:has(.menu-dropdown.shown)", function () {
-      $(".menu-dropdown").removeClass("shown");
-   });
+  $(function () {
+    $(".menu-dropdown-show a").hover(function () {
+      $(".menu-dropdown").addClass("shown");
+    });
+  });
+  $(document).on("mouseleave", "header:has(.menu-dropdown.shown)", function () {
+    $(".menu-dropdown").removeClass("shown");
+  });
 })(jQuery);
